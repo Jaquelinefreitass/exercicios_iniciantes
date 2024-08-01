5b54478686251edeb5573a270b5532389f67a330
Jaqueline <133174675+jueslyyy@users.noreply.github.com>
2024-08-01 14:47:13 -0300

    correção

--- informacao_js/dianamico.js
+++
@@ -1,5 +0,0 @@
-// o js é dinamico por permitir uma msm variavel ter diferentes tipos, e sendo 
-
-// assim se chama dinamico por uma msm variavel assume multiplos valores de tipos diferentes
-
-// então tipagem fraca é dinamica e tipagem forte é statica
---
+++ informacao_js/dinamico.js
@@ -0,0 +1,20 @@
+// o js é dinamico por permitir uma msm variavel ter diferentes tipos, e sendo 
+
+// assim se chama dinamico por uma msm variavel assume multiplos valores de tipos diferentes
+
+// então tipagem fraca é dinamica e tipagem forte é statica
+
+/* TYPESCRIPT
+===========
+É um surper conjunto de JavaScript, tudo que o JS tem  o TS tb tem 
+porque no final o ts vai ser convertido em js, usando TS vc consegue ter
+programas mas escalaveis(mas seguro),com mas segurança q JS n tem .
+Ele vai fazer checagem para ver se o codigo esta correto, é uma linguagem 
+fortemente tipada,seguinifica que quando vc define uma variável vc coloca um tipo 
+para ela EX: uma variavel número, então vc n vai conseguir colocar um texto 
+em uma variável q foi definida para armazenar apenas números
+
+
+
+
+*/
